8ae26120ce65b5f47b147f8463826861d43a257d
Michael Landry <michaellandry@protonmail.com>
2026-03-15 11:13:20 -0400

    projectiles

--- script.js
+++ script.js
@@ -17,13 +17,48 @@ class Player {
         if(this.game.keys.indexOf('d') > -1 || this.game.keys.indexOf('ArrowRight') > -1) this.xPos += this.speed;
 
         //X Boundaries
-        if(this.xPos < 0) this.xPos = 0;
-        else if(this.xPos + this.width > this.game.width) this.xPos = this.game.width - this.width;
+        if(this.xPos < -this.width * 0.5) this.xPos = -this.width * 0.5;
+        else if(this.xPos + this.width > this.game.width) this.xPos = this.game.width - this.width * 0.5;
+    }
+
+    shoot() {
+        const projectile = this.game.getProjectile();
+        if(projectile) projectile.start(this.xPos + this.width * 0.5, this.yPos);
     }
 }
 
 class Projectile {
+    constructor() {
+        this.width = 8;
+        this.height = 20;
+        this.xPos = 0;
+        this.yPos = 0;
+        this.speed = 20;
+        this.free = true;
+    }
 
+    draw(context) {
+        if(!this.free) {
+            context.fillRect(this.xPos, this.yPos, this.width, this.height);
+        }
+    }
+
+    update() {
+        if(!this.free) {
+            this.yPos -= this.speed;
+            if(this.yPos < -this.height) this.reset();
+        }
+    }
+
+    start(xPos, yPos) {
+        this.xPos = xPos - this.width * 0.5; //offset so always in center of player
+        this.yPos = yPos;
+        this.free = false;
+    }
+
+    reset() {
+        this.free = true;
+    }
 }
 
 class Enemy {
@@ -37,11 +72,16 @@ class Game {
         this.height = this.canvas.height;
         this.player = new Player(this);
         this.keys = [];
+        this.projectiles = [];
+        this.numberOfProjectiles = 10;
+        this.createProjectiles();
 
         //Event Listeners
         window.addEventListener('keydown', event => {
             if(this.keys.indexOf(event.key) !== -1) return;
             this.keys.push(event.key);
+            console.log(`Key: ${event.key}\nCode: ${event.code}`);
+            if(event.code === 'Space') this.player.shoot();
         });
 
         window.addEventListener('keyup', event => {
@@ -53,6 +93,24 @@ class Game {
     render(context) {
         this.player.draw(context);
         this.player.update();
+        this.projectiles.forEach(p => {
+                p.update();
+                p.draw(context);
+            }
+        );
+    }
+
+    //Create Projectiles
+    createProjectiles() {
+        for(let i = 0; i < this.numberOfProjectiles; i++) {
+            this.projectiles.push(new Projectile());
+        }
+    }
+
+    getProjectile() {
+        for(let i = 0; i < this.projectiles.length; i++) {
+            if(this.projectiles[i].free) return this.projectiles[i];
+        }
     }
 }
 
